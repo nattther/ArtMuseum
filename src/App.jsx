c274c8707cfa7c318dd208eab0c2e5d62209c7ad
Nathan Tournant <115212705+nattther@users.noreply.github.com>
2024-12-02 16:41:27 +0100

    feat : Image are display 2 by two and responsive.

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,12 @@ import PaintingDisplay from './PaintingDisplay';
 function App() {
   return (
     <>
-
+<div className="ImageContenaires">
       <PaintingDisplay /> 
+      <PaintingDisplay /> 
+      <PaintingDisplay /> 
+      </div>
+
     </>
   );
 }
--- src/PaintingDisplay.jsx
+++ src/PaintingDisplay.jsx
@@ -3,9 +3,11 @@ import styles from './PaintingDisplay.module.css';
 function PaintingDisplay() {
     const imageUrl = "https://lh3.googleusercontent.com/qEnlrp5MyHgLIVvrQR3HYtMBhQaLsxCmhBB15DCxX07l_rAvKqjKAXgCkgigYYxA2hGls9riG6Xfn_K_V5_GMfd_0bE=s0"; 
     return (
+        <div className={styles.ImageContenaire}>
         <img className={styles.Imagepaint}
             src={imageUrl} 
         />
+        </div>
     );
 }
 
