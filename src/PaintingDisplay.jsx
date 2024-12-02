3129a03330ca04b87ba0bd94a07afc949dc903a9
Nathan Tournant <115212705+nattther@users.noreply.github.com>
2024-12-02 16:57:59 +0100

    feat: Add Title and CSS with it.

--- src/PaintingDisplay.jsx
+++ src/PaintingDisplay.jsx
@@ -3,12 +3,16 @@ import styles from './PaintingDisplay.module.css';
 function PaintingDisplay() {
     const imageUrl = "https://lh3.googleusercontent.com/qEnlrp5MyHgLIVvrQR3HYtMBhQaLsxCmhBB15DCxX07l_rAvKqjKAXgCkgigYYxA2hGls9riG6Xfn_K_V5_GMfd_0bE=s0"; 
     const author = "Adriaen Coorte"
+    const titre = "Still Life with Asparagus"
     return (
         <div className={styles.ImageContenaire}>
         <img className={styles.Imagepaint}
             src={imageUrl} 
         />
+        <div className={styles.InfoContenaire}>
+        <p>{titre}</p>
         <p>{author}</p>
+        </div>
         </div>
     );
 }
