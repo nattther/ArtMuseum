854b6c1a20595186e6f058295872542ce9ca781f
Nathan Tournant <115212705+nattther@users.noreply.github.com>
2024-12-02 16:30:46 +0100

    feat : Create PaintingDisplay component.

---
+++ src/PaintingDisplay.jsx
@@ -0,0 +1,13 @@
+import styles from './PaintingDisplay.module.css';
+
+function PaintingDisplay() {
+    const imageUrl = "https://lh3.googleusercontent.com/qEnlrp5MyHgLIVvrQR3HYtMBhQaLsxCmhBB15DCxX07l_rAvKqjKAXgCkgigYYxA2hGls9riG6Xfn_K_V5_GMfd_0bE=s0"; 
+    return (
+        <img className={styles.Imagepaint}
+            src={imageUrl} 
+        />
+    );
+}
+
+export default PaintingDisplay;
+
