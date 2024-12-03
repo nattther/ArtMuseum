95c6aee9fdd5167e7fb1cd841136b0a6cd02fa41
Nathan Tournant <115212705+nattther@users.noreply.github.com>
2024-12-03 14:49:48 +0100

    BREAKING CHANGE :  Refactor for 1 map

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,17 @@
-import './App.css';
-import PaintingDisplay from './PaintingDisplay';
-import data from './infosMockAPI.json';
+import "./App.css";
+import PaintingDisplay from "./PaintingDisplay";
+import data from "./infosMockAPI.json";
 
 function App() {
-  const paintings = data.artObjects.map((item) => ({
-    imageUrl: item.webImage.url,
-    author: item.principalOrFirstMaker,
-    title: item.title,
-    orientation: item.width > item.heigth?  'paysage' : 'portrait'
-  }));
-
   return (
     <div className="ImageContenaires">
-      {paintings.map((painting, index) => (
-          <PaintingDisplay
+      {data.artObjects.map((item, index) => (
+        <PaintingDisplay
           key={index}
-          imageUrl={painting.imageUrl}
-          author={painting.author}
-          title={painting.title}
-          orientation={painting.orientation}
+          imageUrl={item.webImage.url}
+          author={item.principalOrFirstMaker}
+          title={item.title}
+          orientation={item.width > item.height ? "paysage" : "portrait"}
         />
       ))}
     </div>
